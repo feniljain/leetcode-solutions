aba196c4d0330851ccbf6240f08123ff0d2d5f5b
vi_mi <49019259+feniljain@users.noreply.github.com>
2021-04-22 00:57:06 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (35.58%) - LeetHub

---
+++ minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int min = INT_MAX;
+    TreeNode* preorderSuccessor(TreeNode* root) {
+        if(!root) {
+            return nullptr;
+        }
+        
+        TreeNode* node =  preorderSuccessor(root->right);
+        if(node == nullptr) {
+            return root;
+        } else {
+            return node;
+        }
+    }
+    TreeNode* postorderSuccessor(TreeNode* root) {
+        if(!root) {
+            return nullptr;
+        }
+        
+        TreeNode* node =  postorderSuccessor(root->left);
+        if(node == nullptr) {
+            return root;
+        } else {
+            return node;
+        }
+    }
+    int minDiffInBST(TreeNode* root) {
+        if(!root) {
+            return min;
+        }
+            TreeNode* prev = preorderSuccessor(root->left);
+        if(prev) {
+            cout<<root->val<<" - "<<prev->val<<endl;
+         if(abs(root->val-prev->val) < min) {
+            min = abs(root->val-prev->val);
+         }
+        }
+            TreeNode* post = postorderSuccessor(root->right);
+            
+                if(post) {
+                    cout<<root->val<<" : "<<post->val<<endl;
+                    if(abs(root->val-post->val) < min) {
+            min = abs(root->val-post->val);
+         }
+                }
+        
+        minDiffInBST(root->left);
+        minDiffInBST(root->right);
+        
+        return min;
+    }
+};
